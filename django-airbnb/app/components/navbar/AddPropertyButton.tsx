0071b592bfcbb77003309de04f5539a52e8aa42b
Loren Ndung'u <ndunguloren96@gmail.com>
2025-10-07 17:49:21 +0300

    feat: implement navigation components

---
+++ django-airbnb/app/components/navbar/AddPropertyButton.tsx
@@ -0,0 +1,9 @@
+const AddPropertyButton = () => {
+    return (
+        <div className="cursor-pointer p-2 test-sm font-semibold rounded-full hover:bg-gray-200">
+            Django Your Home
+        </div>
+    )
+}
+
+export default AddPropertyButton;
