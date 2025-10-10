a85662709c27e386202366d67d8b4be6a85bbea5
Loren Ndung'u <ndunguloren96@gmail.com>
2025-10-10 23:29:18 +0300

    feat: setup static user menu

--- django-airbnb/app/components/navbar/UserNav.tsx
+++ django-airbnb/app/components/navbar/UserNav.tsx
@@ -1,7 +1,18 @@
+'use client';
+
+import { useState } from "react";
+
+import MenuLink from "./MenuLink";
+
 const UserNav = () => {
+    const [isOpen, setIsOpen] = useState(false)
+    
     return (
         <div className="p-2 relative inline-block border rounded-full">
-            <button className="flex items-center"> 
+            <button 
+                onClick={() => setIsOpen(!isOpen)}
+                className="flex items-center"
+            > 
                 <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                  </svg>
@@ -10,6 +21,23 @@ const UserNav = () => {
                  </svg>
                  
             </button>
+
+            {isOpen && (
+                <div className="w-[220px] absolute top-[60px] right-0 bg-white-200 border rounded-xl shadow-md text-center flex flex-col cursor-pointer">
+                    <MenuLink 
+                        label="Log in"
+                        onClick={() => console.log("Log in")}
+
+                    />
+                    <MenuLink 
+                        label="Sign up"
+                        onClick={() => console.log("Sign up")}
+
+                    />
+
+                </div>
+            
+            )}
         </div>
     )
 }
