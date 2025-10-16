c88fa0b6129a2378232eb3911d1a92d179e9c5b4
Loren Ndung'u <ndunguloren96@gmail.com>
2025-10-16 11:00:40 +0300

    feat: setup docker postgres and DRF, CORS, and Headers

--- airbnb_backend/airbnb_backend/settings.py
+++ airbnb_backend/airbnb_backend/settings.py
@@ -64,11 +64,14 @@ WSGI_APPLICATION = 'airbnb_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get("SQL_ENGINE"),
+        'NAME': os.environ.get("SQL_DB"),
+        'USER': os.environ.get("SQL_USER"),
+        'PASSWORD': os.environ.get("SQL_PASSWORD"),
+        'HOST': os.environ.get("SQL_HOST"),
+        'PORT': os.environ.get("SQL_PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
